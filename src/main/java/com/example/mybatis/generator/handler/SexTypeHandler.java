4d0544f8d7b237b49e6d3804f016c6ae4cecdd96
zhuyanv587 <1677391824@qq.com>
2020-04-12 15:34:20 +0800

    mybatis-three

---
+++ src/main/java/com/example/mybatis/generator/handler/SexTypeHandler.java
@@ -0,0 +1,61 @@
+package com.example.mybatis.generator.handler;
+
+import com.example.mybatis.generator.enumeration.SexEnum;
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.type.MappedJdbcTypes;
+import org.apache.ibatis.type.MappedTypes;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+// 声明JdbcType为整形
+@MappedJdbcTypes(JdbcType.INTEGER)
+
+// 声明JavaType为SexEnum
+@MappedTypes(value= SexEnum.class)
+
+public class SexTypeHandler extends BaseTypeHandler<SexEnum> {
+
+    // 设置非空性别参数
+    @Override
+    public void setNonNullParameter(PreparedStatement ps, int idx,
+                                    SexEnum sex, JdbcType jdbcType) throws SQLException {
+        ps.setInt(idx, sex.getId());
+    }
+
+    // 通过列名读取性别
+    @Override
+    public SexEnum getNullableResult(ResultSet rs, String col) throws SQLException {
+        int id = rs.getInt(col);
+        if (id != 1 && id != 2) {
+            return null;
+        }
+        return SexEnum.getEnumById(id);
+    }
+
+    // 通过下标读取性别
+    @Override
+    public SexEnum getNullableResult(ResultSet rs, int idx) throws SQLException {
+        int id = rs.getInt(idx);
+        if (id != 1 && id != 2) {
+            return null;
+        }
+        return SexEnum.getEnumById(id);
+    }
+
+    // 通过存储过程读取性别
+    @Override
+    public SexEnum getNullableResult(CallableStatement cs, int idx) throws SQLException {
+        int id = cs.getInt(idx);
+        if (id != 1 && id != 2) {
+            return null;
+        }
+        return SexEnum.getEnumById(id);
+    }
+}
+
+// 1. 在properties文件中配置handler的扫描包
+//mybatis.type-handlers-package=com.example.mybatis.generator.handler.SexTypeHandler
---
+++ src/test/java/com/example/mybatis/generator/mapper/MessageMapperTest.java
@@ -0,0 +1,103 @@
+package com.example.mybatis.generator.mapper;
+
+import com.example.mybatis.generator.model.Message;
+import com.example.mybatis.generator.model.MessageExample;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+
+@Slf4j
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class MessageMapperTest {
+    @Resource
+    private MessageMapper messageMapper;
+
+    @Test
+    @Order(1)
+    public void testInsert() {
+        // insert into message(msg_text, msg_summary) values (#{msgText}, #{msgSummary})
+        Message message = new Message();
+        message.setMsgText("orange");
+        message.setMsgSummary("水果");
+        int num = messageMapper.insertSelective(message);
+        log.info("插入的数据条数： {}", num);
+    }
+
+//    @Test
+//    @Order(2)
+//    public void testBatchInsert() {
+//        List<Message> messages = new ArrayList<>(Arrays.asList(
+//                new Message().withMsgText("niit").withMsgSummary("学校"),
+//                new Message().withMsgText("niit").withMsgSummary("学校")));
+//        int num = messageMapper.batchInsert(messages);
+//        log.info("插入的数据条数： {}", num);
+//    }
+
+    @Test
+    @Order(3)
+    public void testSelectAll() {
+        // select * from message
+        messageMapper.selectByExample(null).forEach(
+                message -> log.info("查询的数据： {}", message.toString()));
+    }
+
+    @Test
+    @Order(4)
+    public void testUpdate() {
+        // update message set msg_text = #{msgText}, msg_summary = #{msgSummary} where msg_id = #{msgId}
+        Message message = new Message();
+        message.setMsgId(1);
+        message.setMsgText("computer");
+        message.setMsgSummary("学校");
+        int num = messageMapper.updateByPrimaryKey(message);
+        log.info("更新的数据条数： {}", num);
+    }
+
+    @Test
+    @Order(5)
+    public void testUpdateText() {
+        // update message set msg_text = #{msgText} where msg_id = #{msgId}
+        Message message = new Message();
+        message.setMsgText("computer");
+        MessageExample messageExample = new MessageExample();
+        messageExample.createCriteria().andMsgSummaryLike("水果" +
+                "");
+        int num = messageMapper.updateByExampleSelective(message, messageExample);
+        log.info("更新Text的数据条数： {}", num);
+    }
+
+    @Test
+    @Order(6)
+    public void testSelectById() {
+        // select * from message where msg_id = #{msgId}
+        MessageExample messageExample = new MessageExample();
+        messageExample.createCriteria().andMsgIdEqualTo(8);
+        Message message = messageMapper.selectByExample(messageExample).get(0);
+        log.info("id为1的数据： {}", message);
+    }
+
+    @Test
+    @Order(7)
+    public void testFindByCondition() {
+        // select * from message where msg_text = #{msgText}
+        MessageExample messageExample = new MessageExample();
+        messageExample.createCriteria().andMsgSummaryEqualTo("学校");
+        Message message = messageMapper.selectByExample(messageExample).get(0);
+        log.info("id为1的数据： {}", message);
+    }
+    @Test
+    @Order(8)
+    public void testDelete() {
+        // delete from message where msg_id = #{msgId}
+        MessageExample messageExample = new MessageExample();
+        messageExample.createCriteria().andMsgIdEqualTo(2);
+        int num = messageMapper.deleteByExample(messageExample);
+        log.info("删除的数据条数： {}", num);
+    }
+}
